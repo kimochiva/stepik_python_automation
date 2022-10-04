8c1c3d9f8658c8c8cee79cef32e8e0f17ee58e22
Anne <kimochiva@gmail.com>
2022-10-05 01:06:28 +0300

    added files from previous lessons

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+
+@pytest.fixture(scope="function", autouse=True)
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome(ChromeDriverManager().install())
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
