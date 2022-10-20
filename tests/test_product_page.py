5f1cfed54bad715f94774983013b1b60655b2b5e
Anne <kimochiva@gmail.com>
2022-10-21 00:44:39 +0300

    added parametrization for different promolinks

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -4,10 +4,13 @@ from pages.product_page import ProductPage
 from pages.login_page import LoginPage
 import pytest
 
+@pytest.mark.parametrize('promo', ["offer0", "offer1", "offer2", "offer3", "offer4", "offer5",
+                                   "offer6", pytest.param("offer7", marks=pytest.mark.xfail),
+                                   "offer8", "offer9", "offer10"])
 
-def test_add_item_to_basket(browser):
+def test_add_item_to_basket(browser, promo):
     #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=" + promo
     page = ProductPage(browser, link)
     page.open()
     product_title = page.get_product_title()
@@ -16,4 +19,4 @@ def test_add_item_to_basket(browser):
     page.solve_quiz_and_get_code()
     page.check_basket_product_title(product_title)
     page.check_basket_product_price(product_price)
-    time.sleep(5)
+
